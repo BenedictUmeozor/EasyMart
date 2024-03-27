d3f53b365a21d11fd5340ffe59321c98ac89436e
Benedict <benedictumeozor@gmail.com>
2024-03-27 09:18:03 +0100

    resolving type conflicts

--- app/(client)/cart/CartTable.tsx
+++ app/(client)/cart/CartTable.tsx
@@ -6,12 +6,13 @@ import CartItem from "./CartItem";
 import Link from "next/link";
 import { arraysAreEqual } from "@/utils/functions";
 import toast from "react-hot-toast";
+import { CartItem as CartItemType } from "@/types/types";
 
 const CartTable = memo(() => {
   const { cart, updateCart } = useCartContext();
   const [cartList, setCartList] = useState(cart);
 
-  const editCart = (product: CartItem) => {
+  const editCart = (product: CartItemType) => {
     const newCart = cartList.map((c) => {
       if (c.id === product.id) {
         return product;
@@ -35,7 +36,9 @@ const CartTable = memo(() => {
 
   return (
     <>
-    <h2 className="md:hidden text-center mb-4 text-xl">Cart ({cart.length})</h2>
+      <h2 className="md:hidden text-center mb-4 text-xl">
+        Cart ({cart.length})
+      </h2>
       <table className="w-full max-md:border-collapse">
         <thead className="hidden md:block">
           <tr className="flex flex-col md:grid grid-cols-6 gap-2 text-left mb-4 shadow-sm rounded py-4 px-4 md:items-center">
